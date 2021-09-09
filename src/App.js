80f7e45b40c186c91ed0142d7aae492fcff8617d
zhamkovv <zhamkov@gmail.com>
2021-09-09 21:58:08 +0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn3.f-cdn.com/contestentries/114457/10540864/5446245b3d5f7_thumb900.jpg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
